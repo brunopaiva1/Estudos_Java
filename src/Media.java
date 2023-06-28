70beb072f9c4c1d1d6e81cedeb2c96fb84078727
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-06-27 22:14:41 -0300

    kk

--- src/Media.java
+++ src/Media.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Media {
    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int n = 0;
-        for(int i = 0; i < n; i++){
-            
+        float soma = 0;
+        for(int i = 0; i < 3; i++){           
+            System.out.println("Digite sua nota:");
+            double n = scanner.nextDouble();
+            soma += n;
         }
+        double media = soma / 3;
+        System.out.println("Sua média é " + media);
    } 
 }
