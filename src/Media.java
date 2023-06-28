862eb18e6a3b1aca91bfaa8d6e4b07fc5d4b0d0d
brunopaiva1 <brunopaivamaster10@gmail.com>
2023-06-27 21:58:28 -0300

    .

--- src/Media.java
+++ src/Media.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Media {
    public static void main(String[] args){
-    
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        for(int i = 0; i < n; i++){
+            
+        }
    } 
 }
